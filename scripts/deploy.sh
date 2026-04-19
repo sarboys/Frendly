4898243e0cf6dbcdf36f7ee8af07d38be8c8556a
Sergey Polyakov <sergpol88@gmail.com>
2026-04-19 11:23:21 +0300

    Trim duplicate compose cycle

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,5 +29,5 @@ docker compose --env-file "$ENV_FILE" -f "$COMPOSE_FILE" up minio-init
 docker compose --env-file "$ENV_FILE" -f "$COMPOSE_FILE" up --build migrate
 docker compose --env-file "$ENV_FILE" -f "$COMPOSE_FILE" up -d --build api chat worker
 docker compose --env-file "$ENV_FILE" -f "$COMPOSE_FILE" up -d --force-recreate nginx
-docker compose --env-file "$ENV_FILE" -f "$COMPOSE_FILE" up -d --remove-orphans
+docker compose --env-file "$ENV_FILE" -f "$COMPOSE_FILE" up -d --remove-orphans api chat worker nginx
 docker compose --env-file "$ENV_FILE" -f "$COMPOSE_FILE" ps
