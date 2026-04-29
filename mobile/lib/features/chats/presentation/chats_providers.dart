98c715194095eb0d9b71c725caffb832edeb47b2
Sergey Polyakov <sergpol88@gmail.com>
2026-04-29 14:24:11 +0300

    fix: убрать mobile из root репозитория

--- mobile/lib/features/chats/presentation/chats_providers.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-enum ChatSegment { meetup, personal }
-
-final chatSegmentProvider =
-    StateProvider<ChatSegment>((ref) => ChatSegment.meetup);
